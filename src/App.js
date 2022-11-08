42821938e6d86e20ff19bec25bed61b540987ce2
Raymond Chen <raychen358@gmail.com>
2022-11-07 21:15:06 -0800

    router-dom switches to img

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import Header from './Components/Header'
 import Main from './Components/Main'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-      <Header/>
-      <hr></hr>
-      <Main/>
+    <div className='flex flex-col'>
+      <Router>
+        <Header/>
+        <hr></hr>
+        <Routes>
+          <Route path='/game' element={<Main/>}/>
+        </Routes>
+      </Router>
     </div>
   )
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import Waldo from '../Images/Waldo.jpg'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
 
     return (
-        <div className='flex items-center justify-around gap-4 text-white'>
-            <h1 class='flex items-center'>
-                <img src={Waldo} alt='Waldo' class='h-[10vh]'/>
-                <span class='text-blue-500'>Where's</span>
-                <span class='text-red-500'>Waldo</span>
+        <div class='flex items-center justify-around gap-4 text-white'>
+            <h1 class='flex items-center gap-8'>
+                <div class='flex items-center'>
+                    <img src={Waldo} alt='Waldo' class='h-[10vh]' />
+                    <span class='text-blue-500'>Where's</span>
+                    <span class='text-red-500'>Waldo</span>
+                </div>
+                <Link to='/game'>
+                    <button class='text-black bg-[#1da1f2] w-20 rounded-xl
+          hover:-translate-y-1 hover:scale-110  hover:text-red-400'> Start Game </button>
+                </Link>
             </h1>
         </div>
     )
