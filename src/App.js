fb558a8da729589c5ac251fc4530ffc2140a8068
Raymond Chen <raychen358@gmail.com>
2022-12-06 20:33:11 -0800

    removed header and added in hooks

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React from 'react'
 import Header from './Components/Header'
 import Main from './Components/Main'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, } from "react-router-dom";
+
+import { useEffect, useState } from 'react';
+import { levelData } from './Utils/LevelData';
+import Card from './Components/Card';
+import Home from './Pages/Home';
 
 const App = () => {
+  const [level, setLevel] = useState([])
+  const [gameStart, setGameStart] = useState(false)
+
+
+  const startGame = () => setGameStart(true);
   return (
-    <div className='flex flex-col'>
+    <div className='container px-2 pt-2 pb-4 mx-auto lg:px-2 lg:pt-8'>
       <Router>
-        <Header/>
-        <hr></hr>
-        <Routes>
-          <Route path='/game' element={<Main/>}/>
+        <hr className='mb-4'></hr>
+        <Routes >
+          <Route path='/' element={<Home gameStart={gameStart} startGame={startGame} setLevel={setLevel} levelData={levelData} />} />
         </Routes>
       </Router>
     </div>
