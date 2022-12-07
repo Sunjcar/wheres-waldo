02857524114086f02d18ff59f965552507cf7d2c
Raymond Chen <raychen358@gmail.com>
2022-12-06 22:54:38 -0800

    Updated header with Icons and links

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,21 +1,81 @@
 import React from 'react'
 import Waldo from '../Images/Waldo.jpg'
 import { Link } from 'react-router-dom'
+import { Box, AppBar, Toolbar, IconButton, Tooltip } from "@mui/material";
+import LeaderboardIcon from "@mui/icons-material/Leaderboard";
+import InfoIcon from "@mui/icons-material/Info";
+import HomeIcon from "@mui/icons-material/Home";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
-const Header = () => {
-
+const Header = ({ icon, timer, button }) => {
     return (
         <div class='flex items-center justify-around gap-4 text-white'>
             <h1 class='flex items-center gap-8'>
                 <div class='flex items-center'>
                     <img src={Waldo} alt='Waldo' class='h-[10vh]' />
-                    <span class='text-blue-500'>Where's</span>
-                    <span class='text-red-500'>Waldo</span>
+                    <span class='text-blue-500 text-3xl'>Where's</span>
+                    <span class='text-red-500 text-3xl'>Waldo</span>
                 </div>
-                <Link to='/game'>
-                    <button class='text-black bg-[#1da1f2] w-20 rounded-xl
-          hover:-translate-y-1 hover:scale-110  hover:text-red-400'> Start Game </button>
-                </Link>
+                <AppBar class='sticky'>
+                    <Toolbar
+                        sx={{
+                            height: 80,
+                            display: "flex",
+                            mx: 8,
+                            justifyContent: "space-between",
+                        }}
+                    >
+                        <Box
+                            sx={{ display: "flex", alignItems: "center" }}
+                            component={Link}
+                            to="/"
+                        >
+                        </Box>
+                        <Box
+                            sx={{
+                                display: "flex",
+                                alignItems: "center",
+                                columnGap: "16px",
+                            }}
+                        >
+                            {icon}
+                            {timer}
+                            {button}
+                        </Box>
+                        <Box>
+                            <Tooltip title="Home">
+                                <IconButton color="info" component={Link} to="/">
+                                    <HomeIcon sx={{ fontSize: 40 }} />
+                                </IconButton>
+                            </Tooltip>
+                            <Tooltip title="Leaderboard">
+                                <IconButton color="info" component={Link} to="/leaderboard">
+                                    <LeaderboardIcon sx={{ fontSize: 40 }} />
+                                </IconButton>
+                            </Tooltip>
+                            <Tooltip title="About">
+                                <IconButton
+                                    component="a"
+                                    target="_blank"
+                                    href="https://en.wikipedia.org/wiki/Where's_Wally%3F"
+                                    color="info"
+                                >
+                                    <InfoIcon sx={{ fontSize: 40 }} />
+                                </IconButton>
+                            </Tooltip>
+                            <Tooltip title="Github">
+                                <IconButton
+                                    component="a"
+                                    target="_blank"
+                                    href="https://github.com/sunjcar"
+                                    color="info"
+                                >
+                                    <GitHubIcon sx={{ fontSize: 40 }} />
+                                </IconButton>
+                            </Tooltip>
+                        </Box>
+                    </Toolbar>
+                </AppBar>
             </h1>
         </div>
     )
