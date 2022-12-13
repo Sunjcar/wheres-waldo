ec52f667efd54270db6a8cde544c53b056dc8c54
Raymond Chen <raychen358@gmail.com>
2022-12-12 21:43:20 -0800

    Convert time functions for leaderboard

---
+++ src/Utils/Utils.js
@@ -0,0 +1,37 @@
+export const validateGuess = (guess, location) => {
+    const deviation = 0.01
+    const leftBound = guess.X - (deviation * 2) / 3
+    const rightBound = guess.X + (deviation * 2) / 3
+    const topBound = guess.Y - deviation
+    const bottomBound = guess.Y + deviation
+  
+    if (
+      location.X > leftBound &&
+      location.X < rightBound &&
+      location.Y < bottomBound &&
+      location.Y > topBound
+    )
+      return true
+    return false
+  }
+  
+  export const convertTime = time => {
+    return `${("0" + Math.floor((time / 60000) % 60)).slice(-2)}:${(
+      "0" + Math.floor((time / 1000) % 60)
+    ).slice(-2)}:${("0" + ((time / 10) % 100)).slice(-2)}`
+  }
+  
+  export const getDate = () => {
+    const today = new Date()
+    const dd = String(today.getDate()).padStart(2, "0")
+    const mm = String(today.getMonth() + 1).padStart(2, "0") //January is 0!
+    const yyyy = today.getFullYear()
+  
+    return yyyy + "-" + mm + "-" + dd
+  }
+  
+  export const compareScore = (a, b) => {
+    if (a.time > b.time) return 1
+    else if (a.time < b.time) return -1
+    else return 0
+  }
