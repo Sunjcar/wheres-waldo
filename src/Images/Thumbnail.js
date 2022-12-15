d1b91023ef8715c882f1ee28bf0f432158aca285
Raymond Chen <raychen358@gmail.com>
2022-12-14 20:58:28 -0800

    export images

---
+++ src/Images/Thumbnail.js
@@ -0,0 +1,15 @@
+import beach from "./Levels/beach_thumb.jpg"
+import snow from "./Levels/snow_thumb.jpg"
+import hollywood from "./Levels/hollywood_thumb.jpg"
+import space from "./Levels/space_thumb.jpg"
+import track from "./Levels/track_thumb.jpg"
+import medieval from './Levels/medieval_thumb.jpg'
+
+export const thumbnail = {
+  beach,
+  snow,
+  hollywood,
+  space,
+  track,
+  medieval
+}
